c81e1a8fa447d0098161bc5989130978bf6b27b4
ramadhani20 <80668526+ramadhani20@users.noreply.github.com>
2021-03-23 15:57:45 -1200

    Create Tugas2.java

---
+++ Latihan 2/Tugas2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas2;
+
+/**
+ *
+ * @author DHANI
+ */
+public class Tugas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+                
+    System.out.println("Tugas A:");
+    System.out.println();
+    
+    String[] namalengkap = {"Mohammad", "Rizky", "Ramadhani"};
+    System.out.println( "Namaku adalah " + namalengkap[0] +" "+ namalengkap[1]+" "+namalengkap[2]);
+    
+    
+      
+        
+    }
+   
+}
