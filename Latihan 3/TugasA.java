20a69982184bfdaf66cd6fac51edab4a483cf140
ramadhani20 <80668526+ramadhani20@users.noreply.github.com>
2021-03-31 01:35:37 -1200

    Create TugasA.java

---
+++ Latihan 3/TugasA.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DHANI
+ */
+public class Tugas3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int BatasAwal, BatasAkhir;
+       
+        System.out.println("Masukkan batas awal bilangan : ");
+        BatasAwal = in.nextInt();
+        
+        System.out.println("Masukkan batas akhir bilangan : ");
+         BatasAkhir = in.nextInt();
+                
+        for (int i = BatasAwal; i <= BatasAkhir; i++)    {    
+        
+        System.out.print(" " + i);
+
+        
+        
+}
+}
+}
